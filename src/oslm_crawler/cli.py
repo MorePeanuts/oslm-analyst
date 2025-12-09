1967eb34c6a0295ef0d354bcc8ac7e20711823bb
Leo <95532869+MorePeanuts@users.noreply.github.com>
2025-12-09 16:45:25 +0800

    Add initial skeleton terminal user interface.

--- src/oslm_crawler/cli.py
+++ src/oslm_crawler/cli.py
@@ -1,2 +1,40 @@
+import typer
+from typer import Argument, Option
+from typing import Annotated, Literal
+from datetime import datetime
+
+app = typer.Typer()
+
+
+def today() -> str:
+    return str(datetime.today().date())
+
+
+@app.command()
+def crawl(
+    platform: Annotated[
+        Literal['all', 'huggingface', 'modelscope', 'open-data-lab', 'baai'],
+        Argument(help='Used to specify the platform from which data is to be crawled.'),
+    ] = 'all',
+    max_retry: Annotated[int, Option(help='Maximum number of retries on network error')] = 5,
+):
+    print(platform)
+    print(max_retry)
+
+
+@app.command()
+def rank(
+    date: Annotated[
+        str,
+        Argument(
+            default_factory=today,
+            help='Ranking based on data up to date specified by this argument (today by default).',
+        ),
+    ],
+):
+    print(date)
+
+
 def main() -> None:
     print('Hello from oslm-crawler!')
+    app()
